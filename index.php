76f4aaf54319f2cb5c7c72bc31ae29967b7a7a23
crazyavak <lars.boerboom@outlook.com>
2017-04-23 22:49:11 +0200

    - added stylesheet - added playfield

---
+++ index.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="styles/style.css">
+
+
+	<title>Stratego</title>
+</head>
+<body>
+<table class="playfield">
+	<tr>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+			
+	</tr>
+	<tr>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+			
+	</tr>
+	<tr>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+			
+	</tr>
+	<tr>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+			
+	</tr>
+	<tr>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+			
+	</tr>
+	<tr>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+			
+	</tr>
+	<tr>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+			
+	</tr>
+	<tr>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+			
+	</tr>
+	<tr>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+			
+	</tr>
+	<tr>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+		<td class="playtile"></td>
+			
+	</tr>
+</table>
+</body>
+</html>
